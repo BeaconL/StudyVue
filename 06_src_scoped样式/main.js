217f9c34a69d01b011c18cd6e5281ff5bdd1e690
Baecon_L <fenghuo0203@outlook.com>
2022-06-22 01:24:20 +0800

    067

---
+++ 06_src_scoped样式/main.js
@@ -0,0 +1,15 @@
+// 引入Vue
+import Vue from "vue";
+
+// 引入App
+// eslint-disable-next-line sort-imports
+import App from './App';
+
+// 关闭Vue的生产提示
+Vue.config.productionTip = false;
+
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1 id="hello1" ref="hello2">Hello World</h1>
+        <img ref="img" alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
+        <HelloWorld ref="helloo" msg="Welcome to Your Vue.js App" />
+        <button @click="showDoom">点我打印doom</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from './components/HelloWorld.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    methods: {
+        showDoom() {
+            console.log(document.getElementById("hello1"));
+            console.log(this.$refs.img);
+            console.log(this.$refs.hello);
+            console.log(this.$refs.helloo);
+            console.log(this.$refs.hello2);
+        }
+    }
+};
 </script>
 
 <style>
